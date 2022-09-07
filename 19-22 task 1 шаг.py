83ef3e64b92a5e2b3efcb20607147a38a0566bfa
jur4ikoff <91603315+jur4ikoff@users.noreply.github.com>
2022-09-07 12:51:57 +0300

    Update 19-22 task 1 шаг.py

--- 19-22 task 1 шаг.py
+++ 19-22 task 1 шаг.py
@@ -11,15 +11,18 @@ def game(s):
         return 'W'
     count = 0
 
-    if any(game(move) == 'W' for move in task(s)):
+    if any(game(move) == 'W' for move in task(s)):  # первый петя 
         return 'P1'
 
-    if all(game(move) == 'P1' for move in task(s)):
+    if all(game(move) == 'P1' for move in task(s)):  # первый ваня
         return 'B1'
 
-    if any(game(move) == 'B1' for move in task(s)):
+    if any(game(move) == 'B1' for move in task(s)):  # второй петя 
         return 'P2'
 
+    if all(game(move) == 'P1' or game(move) == 'P2' for move in task(s)):  # второй ваня 
+        return 'B2'
 
-for i in range(20, 80):
+
+for i in range(1, 80):
     print(i, game(i))
